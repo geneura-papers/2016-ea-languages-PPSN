1afd11dec7ee784f043dfc97de9d3ebaed07c5d2
M42 <mromang08@gmail.com>
2016-11-01 21:57:15 +0100

    Documents bitflip.hs

--- haskell/bitflip.hs
+++ haskell/bitflip.hs
@@ -1,31 +1,62 @@
+{-
+bitflip.hs
+Description: Random bitflips over a random boolean vector
+License: GPL-3
+
+This code creates a random boolean vector and applies a fixed number of random
+bitflips over it. The time elapsed during the generation of random positions
+and the application of the bitflips is measured.
+-}
+
+--
+-- This code uses the Data.Sequence data structure to represent the boolean vectors,
+-- which is implemented using Finger Trees, a purely functional data structure
+-- supporting access to the ends in amortized constant time and concatenation and
+-- splitting in time logarithmic in the size of the smaller piece.
+-- 
+-- More information can be found in the following article:
+--    Ralf Hinze and Ross Paterson,
+--    "Finger trees: a simple general-purpose data structure",
+--    Journal of Functional Programming 16:2 (2006) pp 197-217.
+--    http://www.soi.city.ac.uk/~ross/papers/FingerTree.html
+--
 import Data.Sequence hiding (take)
 import Data.Time
-import Control.Applicative
 import Control.DeepSeq
 import System.Random
 
+-- | Number of applications of the bitflip function
+iterations :: Int
 iterations = 100000
 
--- The bitflip function
-bitflip :: Seq Bool -> Int -> Seq Bool
+-- | The bitflip function
+bitflip :: Seq Bool -- ^ Original vector
+        -> Int      -- ^ Position where to apply the bitflip
+        -> Seq Bool
 bitflip v n = update n (not (index v n)) v
 
 
--- The benchmark
-benchmark :: Int -> IO ()
+-- | Complete benchmark over a vector of a given size. It generates a random
+-- vector, starts timing, generates a random vector of positions and applies
+-- a bitflip over those positions.
+benchmark :: Int   -- ^ Lenght of the benchmark vector
+          -> IO ()
 benchmark n = do
   -- Random vector generation
   gen <- newStdGen
   let rbools = randoms gen :: [Bool]
   let vector = fromList $ take n rbools
 
-  -- Timing
+  -- Starts measuring the time
   start <- getCurrentTime
 
   -- Random bitflips
   let rflips = take iterations $ randomRs (0,n-1) gen :: [Int]
   let changedvector = foldl bitflip vector rflips
 
+  -- Stops measuring the time.
+  -- Here 'deepseq' is neccessary to prevent lazy evaluation from giving us
+  -- an incorrect measure.
   stop <- (changedvector `deepseq` getCurrentTime)
   let diffTime = diffUTCTime stop start
 
@@ -34,5 +65,5 @@ benchmark n = do
 
 main :: IO ()
 main = do
-  sequence $ map benchmark ((2^) <$> [4..16])
+  sequence_ $ map benchmark ((2^) <$> [4..16])
   return ()
